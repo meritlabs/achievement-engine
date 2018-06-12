32daae1fd753c623f5a201c73b5620b6f08c8e57
Eugene Sypachev (@Axblade) <eugene@merit.me>
2018-06-12 22:26:08 +0300

    fix migration

--- migrations/data/goals.go
+++ migrations/data/goals.go
@@ -12,11 +12,13 @@ func UpdateGoals(session *mgo.Session) {
 	creatorConditions = append(creatorConditions, models.GoalCondition{
 		Name:        "Create wallet",
 		Description: "",
-		GoalSlug:    1})
+		Slug:        1,
+	})
 	creatorConditions = append(creatorConditions, models.GoalCondition{
 		Name:        "Unlock wallet",
 		Description: "",
-		GoalSlug:    2})
+		Slug:        2,
+	})
 	creator := models.Goal{
 		Slug:        1,
 		Name:        "Creator",
@@ -29,20 +31,24 @@ func UpdateGoals(session *mgo.Session) {
 	var fastStarterConditions []models.GoalCondition
 	fastStarterConditions = append(fastStarterConditions, models.GoalCondition{
 		Name:        "Invite Your Friends to Merit!",
-		Description: "Share your alias with your friends!  Your alias can be used as their invite code when they create a wallet.",
-		GoalSlug:    1})
+		Description: "Share your alias with your friends! Your alias can be used as their invite code when they create a wallet.",
+		Slug:        1,
+	})
 	fastStarterConditions = append(fastStarterConditions, models.GoalCondition{
 		Name:        "Add a friend to your invite waitlist",
 		Description: "You can populate your invite waitlist at any time, even when you don’t have available invites! Just share your alias with your friends and have them use it as their invite code when they create their Merit Wallet.",
-		GoalSlug:    2})
+		Slug:        2,
+	})
 	fastStarterConditions = append(fastStarterConditions, models.GoalCondition{
 		Name:        "Mine an invite",
-		Description: "Invites are randomly distributed among the Merit community with every block that is mined.  Keep an eye out for new invites in your wallet!",
-		GoalSlug:    3})
+		Description: "Invites are randomly distributed among the Merit community with every block that is mined. Keep an eye out for new invites in your wallet!",
+		Slug:        3,
+	})
 	fastStarterConditions = append(fastStarterConditions, models.GoalCondition{
 		Name:        "Confirm an invite request",
 		Description: "Confirm the invite requests pending in your invite waitlist! Remember, invites are scarce!",
-		GoalSlug:    4})
+		Slug:        4,
+	})
 	fastStarter := models.Goal{
 		Slug:        2,
 		Name:        "Fast Starter",
@@ -56,19 +62,23 @@ func UpdateGoals(session *mgo.Session) {
 	tycoonConditions = append(tycoonConditions, models.GoalCondition{
 		Name:        "Send a friend Merit using MeritMoney",
 		Description: "MeritMoney lets you add a password to the transaction or cancel it at any time up until the transaction is accepted. ",
-		GoalSlug:    1})
+		Slug:        1,
+	})
 	tycoonConditions = append(tycoonConditions, models.GoalCondition{
 		Name:        "Chat with the Merit team on Discord or Telegram",
 		Description: "Join us on Discord or Telegram to hear about the latest updates, news, and get answers to any questions you might have about Merit.",
-		GoalSlug:    2})
+		Slug:        2,
+	})
 	tycoonConditions = append(tycoonConditions, models.GoalCondition{
 		Name:        "Receive merit from friend or community",
 		Description: "Your balance should be more than 1MRT.",
-		GoalSlug:    3})
+		Slug:        3,
+	})
 	tycoonConditions = append(tycoonConditions, models.GoalCondition{
 		Name:        "Get SMS notifications",
-		Description: "We can text you and let you know when you get new invites and when you receive MRT.  Just add your number.  ",
-		GoalSlug:    4})
+		Description: "We can text you and let you know when you get new invites and when you receive MRT. Just add your number.",
+		Slug:        4,
+	})
 	tycoon := models.Goal{
 		Slug:        3,
 		Name:        "Merit Tycoon",
@@ -81,8 +91,9 @@ func UpdateGoals(session *mgo.Session) {
 	var growthMasterConditions []models.GoalCondition
 	growthMasterConditions = append(growthMasterConditions, models.GoalCondition{
 		Name:        "Earn your first Growth Reward",
-		Description: "Merit believes in rewarding community members for growing the network.  Adding active users who mine, sell, and invite others will help you earn more Growth Rewards.",
-		GoalSlug:    1})
+		Description: "Merit believes in rewarding community members for growing the network. Adding active users who mine, sell, and invite others will help you earn more Growth Rewards.",
+		GoalSlug:    1,
+	})
 	growthMaster := models.Goal{
 		Slug:        4,
 		Name:        "Growth Master",
