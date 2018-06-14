332b286ec914323f75a66c1d29708862698077fe
Eugene Sypachev (@Axblade) <eugene@merit.me>
2018-06-14 14:33:19 +0300

    disable tycoon and growth master

--- migrations/data/goals.go
+++ migrations/data/goals.go
@@ -58,55 +58,55 @@ func UpdateGoals(session *mgo.Session) {
 		Version:     1,
 	}
 
-	var tycoonConditions []models.GoalCondition
-	tycoonConditions = append(tycoonConditions, models.GoalCondition{
-		Name:        "Send a friend Merit using MeritMoney",
-		Description: "MeritMoney lets you add a password to the transaction or cancel it at any time up until the transaction is accepted. ",
-		Slug:        1,
-	})
-	tycoonConditions = append(tycoonConditions, models.GoalCondition{
-		Name:        "Chat with the Merit team on Discord or Telegram",
-		Description: "Join us on Discord or Telegram to hear about the latest updates, news, and get answers to any questions you might have about Merit.",
-		Slug:        2,
-	})
-	tycoonConditions = append(tycoonConditions, models.GoalCondition{
-		Name:        "Receive merit from friend or community",
-		Description: "Your balance should be more than 1MRT.",
-		Slug:        3,
-	})
-	tycoonConditions = append(tycoonConditions, models.GoalCondition{
-		Name:        "Get SMS notifications",
-		Description: "We can text you and let you know when you get new invites and when you receive MRT. Just add your number.",
-		Slug:        4,
-	})
-	tycoon := models.Goal{
-		Slug:        3,
-		Name:        "Merit Tycoon",
-		Description: "Do more activity in the Merit World",
-		Image:       "achi-tycoon",
-		Conditions:  tycoonConditions,
-		Version:     1,
-	}
+	// var tycoonConditions []models.GoalCondition
+	// tycoonConditions = append(tycoonConditions, models.GoalCondition{
+	// 	Name:        "Send a friend Merit using MeritMoney",
+	// 	Description: "MeritMoney lets you add a password to the transaction or cancel it at any time up until the transaction is accepted. ",
+	// 	Slug:        1,
+	// })
+	// tycoonConditions = append(tycoonConditions, models.GoalCondition{
+	// 	Name:        "Chat with the Merit team on Discord or Telegram",
+	// 	Description: "Join us on Discord or Telegram to hear about the latest updates, news, and get answers to any questions you might have about Merit.",
+	// 	Slug:        2,
+	// })
+	// tycoonConditions = append(tycoonConditions, models.GoalCondition{
+	// 	Name:        "Receive merit from friend or community",
+	// 	Description: "Your balance should be more than 1MRT.",
+	// 	Slug:        3,
+	// })
+	// tycoonConditions = append(tycoonConditions, models.GoalCondition{
+	// 	Name:        "Get SMS notifications",
+	// 	Description: "We can text you and let you know when you get new invites and when you receive MRT. Just add your number.",
+	// 	Slug:        4,
+	// })
+	// tycoon := models.Goal{
+	// 	Slug:        3,
+	// 	Name:        "Merit Tycoon",
+	// 	Description: "Do more activity in the Merit World",
+	// 	Image:       "achi-tycoon",
+	// 	Conditions:  tycoonConditions,
+	// 	Version:     1,
+	// }
 
-	var growthMasterConditions []models.GoalCondition
-	growthMasterConditions = append(growthMasterConditions, models.GoalCondition{
-		Name:        "Earn your first Growth Reward",
-		Description: "Merit believes in rewarding community members for growing the network. Adding active users who mine, sell, and invite others will help you earn more Growth Rewards.",
-		Slug:        1,
-	})
-	growthMaster := models.Goal{
-		Slug:        4,
-		Name:        "Growth Master",
-		Description: "Growth your community!",
-		Image:       "achi-growth-master",
-		Conditions:  growthMasterConditions,
-		Version:     1,
-	}
+	// var growthMasterConditions []models.GoalCondition
+	// growthMasterConditions = append(growthMasterConditions, models.GoalCondition{
+	// 	Name:        "Earn your first Growth Reward",
+	// 	Description: "Merit believes in rewarding community members for growing the network. Adding active users who mine, sell, and invite others will help you earn more Growth Rewards.",
+	// 	Slug:        1,
+	// })
+	// growthMaster := models.Goal{
+	// 	Slug:        4,
+	// 	Name:        "Growth Master",
+	// 	Description: "Growth your community!",
+	// 	Image:       "achi-growth-master",
+	// 	Conditions:  growthMasterConditions,
+	// 	Version:     1,
+	// }
 
 	db := session.DB("achievement-engine").C("goals")
 	db.Upsert(bson.M{"slug": creator.Slug}, creator)
 	db.Upsert(bson.M{"slug": fastStarter.Slug}, fastStarter)
-	db.Upsert(bson.M{"slug": tycoon.Slug}, tycoon)
-	db.Upsert(bson.M{"slug": growthMaster.Slug}, growthMaster)
+	// db.Upsert(bson.M{"slug": tycoon.Slug}, tycoon)
+	// db.Upsert(bson.M{"slug": growthMaster.Slug}, growthMaster)
 
 }
